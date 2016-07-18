62b2a24fcf09af93a5e41dff36455c7cad268358
rostik_dev <rostiknaz@gmail.com>
2016-07-18 19:40:19 +0300

    blog crud

--- app/code/local/Cgi/Blog/controllers/PostController.php
+++ app/code/local/Cgi/Blog/controllers/PostController.php
@@ -14,4 +14,33 @@ class Cgi_Blog_PostController extends Mage_Core_Controller_Front_Action
         $this->loadLayout();
         $this->renderLayout();
     }
+
+    public function createAction()
+    {
+        if(Mage::getSingleton('customer/session')->isLoggedIn()) {
+            $this->loadLayout();
+            $this->renderLayout();
+        } else {
+            $this->_redirect('customer/account/login');
+        }
+    }
+
+    public function updateAction()
+    {
+        $this->loadLayout();
+        $this->renderLayout();
+    }
+
+    public function saveAction()
+    {
+        $data = $this->getRequest()->getParams();
+        $customerData = Mage::getSingleton('customer/session');
+        $post = Mage::getModel('blog/post')
+            ->setPost($data['description'])
+            ->setTitle($data['title'])
+            ->setAuthorId($customerData->getCustomer()->getId());
+        $post->save();
+//        print_r($post);
+        $customerData->addSuccess($this->__('The alert subscription has been saved.'));
+    }
 }
