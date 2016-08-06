0dc7c237d1d2fa53549c170fa1cb46df55cbae7b
rostik_dev <rostiknaz@gmail.com>
2016-08-06 13:56:11 +0300

    task3 : Additional shipping cost -  add totals to invoice and creditmemo

---
+++ app/code/local/Cgi/ShippingCost/Block/Sales/Order/CreditmemoTotals.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: naro
+ * Date: 04.08.16
+ * Time: 17:14
+ */
+class Cgi_ShippingCost_Block_Sales_Order_CreditmemoTotals extends Mage_Sales_Block_Order_Creditmemo_Totals
+{
+
+    /**
+     * Initialize order totals array
+     *
+     * @return Mage_Sales_Block_Order_Totals
+     */
+    protected function _initTotals()
+    {
+        parent::_initTotals();
+        $amount = 0;
+        $orders = $this->getOrder()->getAllItems();
+        foreach($orders as $item){
+            $amount += $item->getAdditionalShippingCost();
+        }
+        if ($amount) {
+            $this->addTotalBefore(new Varien_Object(array(
+                'code'      => 'shippingcost',
+                'value'     => $amount,
+                'base_value'=> $amount,
+                'label'     => $this->helper('shippingcost')->__('Additional shipping'),
+            ), array('shipping', 'tax')));
+        }
+
+        return $this;
+    }
+}
