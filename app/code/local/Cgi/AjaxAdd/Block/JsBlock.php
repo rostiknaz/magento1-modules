429cc50999b48024777c2f9212363002c671a93c
rostik_dev <rostiknaz@gmail.com>
2016-08-10 18:18:13 +0300

    task4 :  ajax add product to cart

---
+++ app/code/local/Cgi/AjaxAdd/Block/JsBlock.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: naro
+ * Date: 14.07.16
+ * Time: 18:16
+ */
+class Cgi_AjaxAdd_Block_JsBlock extends Mage_Catalog_Block_Product_Abstract
+{
+    public function getAddToCartUrl()
+    {
+        return 1;
+    }
+}
---
+++ app/code/local/Cgi/AjaxAdd/controllers/CartController.php
@@ -0,0 +1,88 @@
+<?php
+require_once 'Mage/Checkout/controllers/CartController.php';
+/**
+ * Created by PhpStorm.
+ * User: naro
+ * Date: 10.08.16
+ * Time: 14:43
+ */
+class Cgi_AjaxAdd_CartController extends Mage_Checkout_CartController
+{
+    public function addAction()
+    {
+        $cart   = $this->_getCart();
+        $params = $this->getRequest()->getParams();
+        if($params['isAjax'] == 1){
+            $response = array();
+            try {
+                if (isset($params['qty'])) {
+                    $filter = new Zend_Filter_LocalizedToNormalized(
+                        array('locale' => Mage::app()->getLocale()->getLocaleCode())
+                    );
+                    $params['qty'] = $filter->filter($params['qty']);
+                }
+
+                $product = $this->_initProduct();
+                $related = $this->getRequest()->getParam('related_product');
+
+                /**
+                 * Check product availability
+                 */
+                if (!$product) {
+                    $response['status'] = 'error';
+                    $response['message'] = $this->__('Unable to find Product ID');
+                }
+
+                $cart->addProduct($product, $params);
+                if (!empty($related)) {
+                    $cart->addProductsByIds(explode(',', $related));
+                }
+
+                $cart->save();
+
+                $this->_getSession()->setCartWasUpdated(true);
+
+                /**
+                 * @todo remove wishlist observer processAddToCart
+                 */
+                Mage::dispatchEvent('checkout_cart_add_product_complete',
+                    array('product' => $product, 'request' => $this->getRequest(), 'response' => $this->getResponse())
+                );
+
+                if (!$this->_getSession()->getNoCartRedirect(true)) {
+                    if (!$cart->getQuote()->getHasError()){
+                        $count = Mage::helper('checkout/cart')->getSummaryCount();
+                        $message = $this->__('%s was added to your shopping cart.', Mage::helper('core')->htmlEscape($product->getName()));
+                        $response['status'] = 'success';
+                        $response['message'] = $message;
+                        $response['count'] = $count;
+                        $this->loadLayout();
+                        $minicart = $this->getLayout()->getBlock('minicart_head')->toHtml(); // <-- here's the block
+                        $response['minicart'] = $minicart;
+                    }
+                }
+            } catch (Mage_Core_Exception $e) {
+                $msg = "";
+                if ($this->_getSession()->getUseNotice(true)) {
+                    $msg = $e->getMessage();
+                } else {
+                    $messages = array_unique(explode("\n", $e->getMessage()));
+                    foreach ($messages as $message) {
+                        $msg .= $message.'<br/>';
+                    }
+                }
+
+                $response['status'] = 'error';
+                $response['message'] = $msg;
+            } catch (Exception $e) {
+                $response['status'] = 'error';
+                $response['message'] = $this->__('Cannot add the item to shopping cart.');
+                Mage::logException($e);
+            }
+            return $this->getResponse()->setBody(Mage::helper('core')->jsonEncode($response));
+
+        } else {
+            return parent::addAction();
+        }
+    }
+}
